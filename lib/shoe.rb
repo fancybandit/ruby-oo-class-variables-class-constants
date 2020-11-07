6e1e6fa7ff87fa580bef0cd75d2eddb90fcf57d2
Jacob Klimenko <droidedjake@gmail.com>
2020-11-06 21:17:42 -0500

    commit tree 1f2b144207c20e18f02ffb25c898c5f3db1396b6

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,28 @@
 class Shoe
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+
+  def BRANDS.size
+    brands_list = []
+    brands_list += BRANDS
+    brands_list.each_with_index do |brand, index|
+      matches = BRANDS.select {|i| i == brand}
+      matches.length > 1 ? brands_list.delete_at(index) : nil
+    end
+    brands_list.length
   end
 
 end
